8adf5f4f765288b71810644ce6e6b3711eb1dbc1
yeongdonge <parrineau@gmail.com>
2022-04-12 15:35:39 +0900

    Init Repository

---
+++ src/test/java/exchange/rate/RateApplicationTests.java
@@ -0,0 +1,13 @@
+package exchange.rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
